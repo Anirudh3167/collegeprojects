d4fa1848bdc7df2e59b76d3c9a2a28a5ce5edc1b
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-10-17 16:18:45 +0530

    Create BisectionMethod.cpp
    Bisection method

---
+++ BisectionMethod.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void f(float x) {
+     //Let us assume the equation as x*x*x + x - 1.
+     return x*x*x + x - 1;
+}
+
+int main() {
+     float a;float b;float x;int iter=0;
+     cout << "Lower limit:"; cin >> a;
+     cout << "Upper limit:"; cin >> b;
+     (f(a)==0)? cout << "root is:"+to_string(a):"";
+     (f(b)==0)? cout << "root is:"+to_string(b):"";
+     if ((f(a)!=0)&&(f(b)!=0)) {
+           cout << "iteration limit:"; cin >> iter;
+     }
+     for (int i=0;i<iter;i++) {
+           x = (a+b)/2;
+           (f(x)<0)? a=x:b=x;
+           i=((f(x)>0)&&(f(x)<0.0001)? iter:i);
+     }
+     cout << "The nearest root is:" << x;
