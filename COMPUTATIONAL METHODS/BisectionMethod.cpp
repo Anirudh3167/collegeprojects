a5da0423746ef3c80352088334fc31ab7f2f9732
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-10-20 19:59:32 +0530

    Update BisectionMethod.cpp

--- COMPUTATIONAL METHODS/BisectionMethod.cpp
+++ COMPUTATIONAL METHODS/BisectionMethod.cpp
@@ -7,18 +7,25 @@ float f(float x) {
 }
 
 int main() {
-     float a;float b;float x;int iter=0;
+     float a;float b;float x;int iter=0;string out;
+     cout << "\t\tEquation: x*x*x + x - 1\n\n";
      cout << "Lower limit:"; cin >> a;
      cout << "Upper limit:"; cin >> b;
      cout << ((f(a)==0)?"root is:"+to_string(a):"");
      cout << ((f(b)==0)?"root is:"+to_string(b):"");
+     if (f(a)*f(b) >= 0) {
+         cout << "There is no possible root";
+         exit(0);
+     }
      if ((f(a)!=0)&&(f(b)!=0)) {
            cout << "iteration limit:"; cin >> iter;
      }
+     cout << "iteration\ta\t\tb\t\tx\t\tf(x)\n";
      for (int i=0;i<iter;i++) {
            x = (a+b)/2;
+           cout << to_string(i+1)+"\t\t"+to_string(a)+"\t"+to_string(b)+"\t"+to_string(x)+"\t"+to_string(f(x))+"\n";
            (f(x)<0)? a=x:b=x;
            i=((f(x)>0)&&(f(x)<0.0001)? iter:i);
      }
-     cout << "The nearest root is:" << x;
+     cout << "\nThe nearest root is:" << x;
 }
