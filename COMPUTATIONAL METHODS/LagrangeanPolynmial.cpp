c1491a7af8757de215d06bcba525efce43823eb7
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-11-07 22:09:21 +0530

    Update LagrangeanPolynmial.cpp

--- COMPUTATIONAL METHODS/LagrangeanPolynmial.cpp
+++ COMPUTATIONAL METHODS/LagrangeanPolynmial.cpp
@@ -1,1 +1,49 @@
-Work is going on
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<float> split(string str) {
+    string str1;
+    vector<float> vect;
+    for (int i=0; i < str.length(); i++ ) {
+        if (str[i] != ' ') {
+            str1 = str1 + str[i];
+        }
+        else {
+            vect.push_back(stof(str1));
+            str1 = "";
+        }
+    }
+    vect.push_back(stof(str1));
+    return  vect;
+}
+
+int main()
+{
+    string abc;
+    vector<float> vect1,vect2;
+    int val;
+    cout << "Use space to seperate\n";
+    cout << "X: ";
+    getline(cin,abc);
+    vect1 = split(abc);
+    cout << "F(x): ";
+    getline(cin,abc);
+    vect2 = split(abc);
+    cout << "value of X:";
+    cin >> val;
+    float prod = 1, sum = 0;
+    for (int i = 0; i < vect1.size(); i++) {
+            for (int j = 0; j < vect1.size(); j++) {
+                if (i != j) {
+                    prod = prod*((val - vect1[j])/(vect1[i] - vect1[j]));
+                }
+            }
+            prod = prod*vect2[i];
+            sum = sum + prod;
+            prod = 1;
+    }
+    cout << "Ans:" << sum;
+    return 0;
+}
