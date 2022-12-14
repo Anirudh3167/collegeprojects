92110d3f91544a7278bca1715c3f7cc5a09278d3
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-12-14 09:35:23 +0530

    Create Simpsons_ThreeEighth.cpp

---
+++ COMPUTATIONAL METHODS/Simpsons_ThreeEighth.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<float> split(string str) {
+    string str1;
+    vector<float> vect;
+    string search;
+    for (int i=0; i < str.length(); i++ ) {
+        search = str[i];
+        if (search != "\t") {
+            str1 = str1 + str[i];
+        }
+        else {
+            vect.push_back(stof(str1));
+            str1 = "";
+        }
+    }
+    vect.push_back(stof(str1));
+    return  vect;
+}
+
+int main()
+{
+    string abc;
+    vector<float> vect1,vect2;
+    float val,h;
+    cout << "Use tab to seperate\n";
+    cout << "X:\t";
+    getline(cin,abc);
+    vect1 = split(abc);
+    cout << "F(x):\t";
+    getline(cin,abc);
+    vect2 = split(abc);
+    float sum = 0 , sum1 = 0, triple_sum = 0;
+    h = vect1[1] - vect1[0];
+    for(int i = 0; i < vect2.size(); i++) {
+        if ((i != 0) && (i != vect2.size() - 1)) {
+            if (i%3 == 0) {
+                
+                triple_sum = triple_sum + vect2[i];
+            } else {
+                sum = sum + vect2[i];
+            }
+        } else {
+            sum1 = sum1 + vect2[i];
+        }
+    }
+    val = 3*h*(sum1 + 2*triple_sum + 3*sum)/8;
+    cout << "Ans :" << val;
+    return 0;
+}
